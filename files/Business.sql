5d87c5e63103a248d508695811cf6a80a963d745
Ozmanov Alik <alik.ozmanov@fms-ea.com>
2024-10-23 08:25:34 +0200

    Initial commit

---
+++ files/Business.sql
@@ -0,0 +1,56 @@
+-- Reconstruction de la base de données
+DROP DATABASE IF EXISTS Business;
+
+CREATE DATABASE Business;
+
+USE Business;
+
+-- Construction de la table des catégories
+CREATE TABLE T_Categories (
+    IdCategory     INT PRIMARY KEY AUTO_INCREMENT,
+    CategoryName   VARCHAR(30) NOT NULL
+) ENGINE = InnoDB;
+
+-- Construction de la table des formations
+CREATE TABLE T_Formations (
+    IdFormation    INT PRIMARY KEY AUTO_INCREMENT,
+    Name            VARCHAR(100) NOT NULL,
+    Description    TEXT NOT NULL,
+    DurationDays     INT NOT NULL,
+    FaceToFace     BOOLEAN NOT NULL,
+    Price          DECIMAL(10, 2) NOT NULL,
+    IdCategory     INT,
+    FOREIGN KEY (IdCategory) REFERENCES T_Categories(IdCategory)
+) ENGINE = InnoDB;
+
+-- Insertion des catégories dans la table T_Categories
+INSERT INTO T_Categories (CategoryName) VALUES ('Développement Web');
+INSERT INTO T_Categories (CategoryName) VALUES ('Data Science');
+INSERT INTO T_Categories (CategoryName) VALUES ('Marketing Digital');
+INSERT INTO T_Categories (CategoryName) VALUES ('Design UX/UI');
+
+-- Insertion des formations dans la table T_Formations
+INSERT INTO T_Formations (Name, Description, DurationDays,  FaceToFace , Price, IdCategory) 
+VALUES ('Formation Java', 'Java SE 8 : Syntaxe & Poo', 5, TRUE, 20, 1);
+INSERT INTO T_Formations (Name, Description, DurationDays,  FaceToFace, Price, IdCategory) 
+VALUES ('Formation Java avancé', 'Exceptions, fichiers, Jdbc, thread...', 10, FALSE, 20, 2);
+INSERT INTO T_Formations (Name, Description, DurationDays,  FaceToFace , Price, IdCategory) 
+VALUES ('Formation Spring', 'Spring Core/Mvc/Security', 3, TRUE, 20, 3);
+INSERT INTO T_Formations (Name, Description, DurationDays,  FaceToFace , Price, IdCategory) 
+VALUES ('Formation Php frameworks', 'Symphony', 7, FALSE, 15, 4);
+INSERT INTO T_Formations (Name, Description, DurationDays,  FaceToFace , Price, IdCategory) 
+VALUES ('Formation C#', 'DotNet Core', 7, TRUE, 20, 4);
+
+-- Sélection des formations par catégorie
+SELECT F.IdFormation, F.Name, F.Description, F.DurationDays, F.FaceToFace, F.Price, C.CategoryName 
+FROM T_Formations F 
+JOIN T_Categories C 
+ON F.IdCategory = C.IdCategory;
+
+
+-- Sélection formations contenant un mot clé dans la description (Spring)
+SELECT * FROM T_Formations WHERE Description LIKE '%Spring%';
+
+-- Sélection des formations en présentiel ou distanciel
+SELECT * FROM T_Formations WHERE FaceToFace = TRUE; -- Présentiel
+SELECT * FROM T_Formations WHERE FaceToFace = FALSE; -- Distanciel
---
+++ src/fr/fms/entities/Formation.java
@@ -0,0 +1,51 @@
+package fr.fms.entities;
+
+public class Formation {
+    private int idFormation;
+    private String name;
+    private String description;
+    private int durationDays;
+    private boolean faceToFace;
+    private double price;
+    private String categoryName;
+
+    // Constructeur
+    public Formation(int idFormation, String name, String description, int durationDays, boolean faceToFace, double price, String categoryName) {
+        this.idFormation = idFormation;
+        this.name = name;
+        this.description = description;
+        this.durationDays = durationDays;
+        this.faceToFace = faceToFace;
+        this.price = price;
+        this.categoryName = categoryName;
+    }
+
+    // Getters
+    public int getIdFormation() {
+        return idFormation;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getDurationDays() {
+        return durationDays;
+    }
+
+    public boolean isFaceToFace() {
+        return faceToFace;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+}
